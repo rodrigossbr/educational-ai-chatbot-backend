fc0af44300fbf07717b373d84dbd67ff4fc5bf8a
Rodrigo Silveira dos Santos <rodrigoss.br@gmail.com>
2025-12-07 23:55:38 -0300

    Adicionado gitignore.

--- config/settings.py
+++ config/settings.py
@@ -83,10 +83,6 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': BASE_DIR / 'db.sqlite3',
-    # }
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'chatbot_db',
